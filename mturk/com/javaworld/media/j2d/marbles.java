ea1e8aae9454bbfa01c1407bc54af45687be786f
Nick Kaashoek <nick.kaasheok@gmail.com>
2014-01-13 21:59:37 -0500

    Added a mouselistener to catch if people click on the ellipses we want them to click on

--- mturk/com/javaworld/media/j2d/marbles.java
+++ mturk/com/javaworld/media/j2d/marbles.java
@@ -1,9 +1,12 @@
 package com.javaworld.media.j2d;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
 import java.util.Random;
-
-public class marbles extends Frame{
+public class marbles extends Frame implements MouseListener{
+	public ArrayList<Ellipse2D> goodellipses =  new ArrayList<Ellipse2D>();
 	public static void main(String[] args){
 		new marbles();
 	}
@@ -11,10 +14,20 @@ public class marbles extends Frame{
 		super("Java 2D");
 		setSize(400,300);
 		setVisible(true);
+		this.addMouseListener(this);
 		addWindowListener(new WindowAdapter(){
 			public void windowClosing(WindowEvent e){
 				dispose(); System.exit(0);}
 			});
+		
+	}
+	public void mouseClicked(MouseEvent evt){
+		Point2D point= evt.getPoint();
+		System.out.println("Mouse clicked at "+point);
+		for (Ellipse2D e:goodellipses){
+			if(e.contains(point))
+				System.out.println("Found in ellipse");
+		}
 	}
 	public void paint(Graphics g){
 		Random rand = new Random();
@@ -34,7 +47,31 @@ public class marbles extends Frame{
 		for(int i=0;i<number2;i++){
 			int x = rand.nextInt(400);
 			int y = rand.nextInt(300);
-			g2d.fillOval(x, y, 10, 10);
+			Ellipse2D e = new Ellipse2D.Double(x, y, 10,10);
+			goodellipses.add(e);
 		}
+	    for (Ellipse2D e : goodellipses) {
+	        g2d.draw(e);
+	    }
+	}
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
