6f3d0b33451951152ae5499bda4c6596dec9eb97
willy-vvu <willy.vvu@gmail.com>
2014-01-27 22:41:57 -0500

    Refactored and improved the code
    Ellipses are now in ArrayLists.
    Window will re-render and orange circles will disappear.
    Refactored variables and moved the listener into an anonymous class.
    More to come soon.

--- mturk/com/javaworld/media/j2d/marbles.java
+++ mturk/com/javaworld/media/j2d/marbles.java
@@ -1,86 +1,116 @@
 package com.javaworld.media.j2d;
+
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.Random;
-public class marbles extends Frame implements MouseListener{
-	public ArrayList<Ellipse2D> goodellipses =  new ArrayList<Ellipse2D>();
-	public int score=0;
-	public static void main(String[] args){
-		new marbles();
+
+public class Marbles extends Frame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6806601217744283298L;
+	public ArrayList<Ellipse2D> goodellipses = new ArrayList<Ellipse2D>();
+	public ArrayList<Ellipse2D> badellipses = new ArrayList<Ellipse2D>();
+	public int score = 0;
+
+	public static void main(String[] args) {
+		new Marbles();
 	}
-	public marbles(){
+
+	public Marbles() {
 		super("Java 2D");
-		setSize(400,300);
+		setSize(400, 300);
 		setVisible(true);
-		this.addMouseListener(this);
-		addWindowListener(new WindowAdapter(){
-			public void windowClosing(WindowEvent e){
-				dispose(); System.exit(0);}
-			});
-		
-	}
-	public void mouseClicked(MouseEvent evt){
-		Point2D point= evt.getPoint();
-		System.out.println("Mouse clicked at "+point);
-		for (Ellipse2D e:goodellipses){
-			if(e.contains(point)){
-				score++;
-				System.out.println("Found in ellipse, total score is "+score);
-				int index = goodellipses.indexOf(e);
-				goodellipses.remove(index);
-				if(goodellipses.size()==0){
-					System.out.println("YOU WIN!!!!!");
+		generateEllipses();
+		this.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mousePressed(MouseEvent evt) {
+				// TODO Auto-generated method stub
+				Point2D point = evt.getPoint();
+				System.out.println("Mouse clicked at " + point);
+				for (Ellipse2D e : goodellipses) {
+					if (e.contains(point)) {
+						score++;
+						System.out.println("Found in ellipse, total score is "
+								+ score);
+						int index = goodellipses.indexOf(e);
+						goodellipses.remove(index);
+						if (goodellipses.size() == 0) {
+							System.out.println("YOU WIN!!!!!");
+						}
+						break;
+					}
 				}
-				break;
+				repaint();
 			}
-		}
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				dispose();
+				System.exit(0);
+			}
+		});
+
 	}
-	public void paint(Graphics g){
+
+	private void generateEllipses() {
 		Random rand = new Random();
-		g.setColor(Color.blue);
-		Graphics2D g2da = (Graphics2D)g;
-		g2da.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
-		int number = rand.nextInt(80)+50;
-		for(int i=0;i<number;i++){
+		int number = rand.nextInt(80) + 50;
+		for (int i = 0; i < number; i++) {
 			int x = rand.nextInt(400);
 			int y = rand.nextInt(300);
-			g2da.fillOval(x, y, 10, 10);
+			Ellipse2D e = new Ellipse2D.Double(x, y, 10, 10);
+			badellipses.add(e);
 		}
-		Graphics2D g2d = (Graphics2D)g;
-		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);	    
 		int number2 = rand.nextInt(20);
-		g2d.setColor(Color.orange);
-		for(int i=0;i<number2;i++){
+		for (int i = 0; i < number2; i++) {
 			int x = rand.nextInt(400);
 			int y = rand.nextInt(300);
-			Ellipse2D e = new Ellipse2D.Double(x, y, 10,10);
+			Ellipse2D e = new Ellipse2D.Double(x, y, 10, 10);
 			goodellipses.add(e);
 		}
-	    for (Ellipse2D e : goodellipses) {
-	        g2d.draw(e);
-	    }
+
 	}
-	@Override
-	public void mouseEntered(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mouseExited(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mousePressed(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mouseReleased(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
+
+	public void paint(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		g2d.setColor(Color.blue);
+		for (Ellipse2D e : badellipses) {
+			g2d.fill(e);
+		}
+		g2d.setColor(Color.orange);
+		for (Ellipse2D e : goodellipses) {
+			g2d.draw(e);
+		}
 	}
 }
