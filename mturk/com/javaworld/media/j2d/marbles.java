6b4d85faab3a4901c5fab41510d2a27230b03f2c
willy-vvu <willy.vvu@gmail.com>
2014-02-02 11:05:19 -0500

    Added intro

--- mturk/com/javaworld/media/j2d/marbles.java
+++ mturk/com/javaworld/media/j2d/marbles.java
@@ -1,35 +1,117 @@
 package com.javaworld.media.j2d;
 
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Marbles extends Frame {
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Marbles {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -6806601217744283298L;
-	public ArrayList<Ellipse2D> goodellipses = new ArrayList<Ellipse2D>();
-	public ArrayList<Ellipse2D> badellipses = new ArrayList<Ellipse2D>();
-	public int score = 0;
+	private final static String MAINACTIVITY = "MAINACTIVITY";
+	private final static String INTROACTIVITY = "INTROACTIVITY";
 
 	public static void main(String[] args) {
 		new Marbles();
 	}
 
 	public Marbles() {
-		super("Java 2D");
-		setSize(400, 300);
-		setVisible(true);
+		JFrame main = new JFrame("Java 2D");
+		main.setSize(400, 300);
+		final CardLayout mainCards = new CardLayout();
+		final JPanel mainPanel = new JPanel(mainCards);
+		main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel introPanel=new JPanel(new BorderLayout());
+		introPanel.add(new JLabel("INTRO goes here.",JLabel.CENTER),BorderLayout.CENTER);
+		JButton beginButton=new JButton("Begin!");
+		beginButton.addMouseListener(new MouseListener() {
+			
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				mainCards.show(mainPanel, MAINACTIVITY);
+			}
+		});
+		introPanel.add(beginButton,BorderLayout.PAGE_END);
+		mainPanel.add(introPanel, INTROACTIVITY);
+		mainPanel.add(new MarbleDrawingComponent(), MAINACTIVITY);
+		main.add(mainPanel);
+		main.setVisible(true);
+
+	}
+
+}
+
+class MarbleDrawingComponent extends JComponent {
+	public ArrayList<Ellipse2D> goodellipses = new ArrayList<Ellipse2D>();
+	public ArrayList<Ellipse2D> badellipses = new ArrayList<Ellipse2D>();
+	public int score = 0;
+
+	private void generateEllipses() {
+		Random rand = new Random();
+		int number = rand.nextInt(80) + 50;
+		for (int i = 0; i < number; i++) {
+			int x = rand.nextInt(400);
+			int y = rand.nextInt(300);
+			Ellipse2D e = new Ellipse2D.Double(x, y, 10, 10);
+			badellipses.add(e);
+		}
+		int number2 = rand.nextInt(20);
+		for (int i = 0; i < number2; i++) {
+			int x = rand.nextInt(400);
+			int y = rand.nextInt(300);
+			Ellipse2D e = new Ellipse2D.Double(x, y, 10, 10);
+			goodellipses.add(e);
+		}
+
+	}
+
+	public MarbleDrawingComponent() {
 		generateEllipses();
 		this.addMouseListener(new MouseListener() {
 
 			@Override
 			public void mousePressed(MouseEvent evt) {
-				// TODO Auto-generated method stub
 				Point2D point = evt.getPoint();
 				System.out.println("Mouse clicked at " + point);
 				for (Ellipse2D e : goodellipses) {
@@ -72,35 +154,9 @@ public class Marbles extends Frame {
 
 			}
 		});
-		addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent e) {
-				dispose();
-				System.exit(0);
-			}
-		});
-
 	}
 
-	private void generateEllipses() {
-		Random rand = new Random();
-		int number = rand.nextInt(80) + 50;
-		for (int i = 0; i < number; i++) {
-			int x = rand.nextInt(400);
-			int y = rand.nextInt(300);
-			Ellipse2D e = new Ellipse2D.Double(x, y, 10, 10);
-			badellipses.add(e);
-		}
-		int number2 = rand.nextInt(20);
-		for (int i = 0; i < number2; i++) {
-			int x = rand.nextInt(400);
-			int y = rand.nextInt(300);
-			Ellipse2D e = new Ellipse2D.Double(x, y, 10, 10);
-			goodellipses.add(e);
-		}
-
-	}
-
-	public void paint(Graphics g) {
+	public void paintComponent(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 				RenderingHints.VALUE_ANTIALIAS_ON);
